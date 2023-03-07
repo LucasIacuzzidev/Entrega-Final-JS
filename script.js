799631a1ce2c27082e05db861b9c63f9b0c7ec79
Lucas Iacuzzi <iacuzzidev@gmail.com>
2023-03-07 12:58:15 -0300

    Ingreso de datos por prompt remplazado con dom

--- script.js
+++ script.js
@@ -1,83 +1,82 @@
 // Objeto Deudor // 
 function Deudor(nombre, apellido, totalAdeudado) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.totalAdeudado = totalAdeudado;
-  }
-  
-  // Array de deudores // 
-  const deudores = [];
-  
-  // Función para solicitar los datos del préstamo // 
-  const datosPrestamoForm = document.getElementById("datosPrestamo");
-  function datosPrestamo() {
-    const sueldoMinimo = 100000;
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.totalAdeudado = totalAdeudado;
+}
+
+// Array de deudores // 
+const deudores = [];
+
+// Función para solicitar los datos del préstamo // 
+const datosPrestamoForm = document.getElementById("datosPrestamo");
+function datosPrestamo() {
+  const sueldoMinimo = 100000;
+  datosPrestamoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
     const nombres = document.getElementById("name").value;
     const apellido = document.getElementById("lastName").value;
-    const sueldo = parseFloat(document.getElementById("income").value);
-  
+    let sueldo = parseFloat(document.getElementById("income").value);
+    const montoPrestamo = parseFloat(document.getElementById("prestamoMonto").value);
+    const cantidadMeses = parseInt(document.getElementById("cuotas").value);
+    const montoTotal = simuladorDeIntereses(montoPrestamo, cantidadMeses);
+    const deudor = new Deudor(nombres, apellido, montoTotal);
+    deudores.push(deudor)
     if (sueldo < sueldoMinimo) {
       alert(
         `Lo sentimos ${nombres} ${apellido}, tu ingreso mensual es insuficiente para acceder al préstamo`
       );
       return;
     }
-  
-    let calcularOtroPrestamo = true;
-    while (calcularOtroPrestamo) {
-      const montoPrestamo = parseFloat(prompt("Ingresa el monto del préstamo"));
-      const cantidadMeses = parseInt(prompt("Ingresa el número de meses de retraso (3-6-9-12)"));
-      const montoTotal = simuladorDeIntereses(montoPrestamo, cantidadMeses);
-      const deudor = new Deudor(nombres, apellido, montoTotal);
-      deudores.push(deudor);
-  
-      alert(
-        `Hola ${nombres} ${apellido}, el monto total a pagar por el préstamo es de $${montoTotal}`
-      );
-  
-      calcularOtroPrestamo = confirm("Desea realizar otra simulacion?");
+    alert(
+      `Hola ${nombres} ${apellido}, el monto total a pagar por el préstamo es de $${montoTotal}`
+    );
+    const calcularOtroPrestamo = confirm("Desea realizar otra simulacion?");
+    if (!calcularOtroPrestamo) {
+      const montosAdeudados = deudores.map((deudor) => deudor.totalAdeudado);
+      const montosDeudasGrandes = deudores.filter((deudor) => deudor.totalAdeudado > 50000);
+      console.log("Montos adeudados:", montosAdeudados);
+      console.log("Montos de deudas mayores a 50000:", montosDeudasGrandes);
+      const ultimoDeudor = deudores[deudores.length - 1];
+      console.log(`El último deudor agregado es: ${ultimoDeudor.nombre} ${ultimoDeudor.apellido} con un total adeudado de $${ultimoDeudor.totalAdeudado}`); // Solo el ultimo deudor 
+      console.log(deudores); // Array de deudores (almacena cada simulacion de prestamo.)
     }
-    const montosAdeudados = deudores.map((deudor) => deudor.totalAdeudado);
-    const montosDeudasGrandes = deudores.filter((deudor) => deudor.totalAdeudado > 50000)
-    console.log("Montos adeudados:", montosAdeudados);
-    console.log("Montos de deudas mayores a 50000:", montosDeudasGrandes);
-    const ultimoDeudor = deudores[deudores.length - 1];
-    console.log(`El último deudor agregado es: ${ultimoDeudor.nombre} ${ultimoDeudor.apellido} con un total adeudado de $${ultimoDeudor.totalAdeudado}`); // Solo el ultimo deudor 
-    console.log(deudores); // Array de deudores (almacena cada simulacion de prestamo.)
+    datosPrestamoForm.reset();
+  });
+}
+
+// Función para simular los intereses del préstamo // 
+function simuladorDeIntereses(montoPrestamo, cantidadMeses) {
+  const tasaInteres = 0.08;
+  let montoTotal = montoPrestamo;
+
+  for (let i = 0; i < cantidadMeses; i++) {
+    montoTotal += montoTotal * tasaInteres;
   }
-  
-  // Función para simular los intereses del préstamo // 
-  function simuladorDeIntereses(montoPrestamo, cantidadMeses) {
-    const tasaInteres = 0.08;
-    let montoTotal = montoPrestamo;
-  
-    for (let i = 0; i < cantidadMeses; i++) {
-      montoTotal += montoTotal * tasaInteres;
-    }
-  
-    return Math.ceil(montoTotal);
-  }
-  
-  // Modo Dark-Light 
+
+  return Math.ceil(montoTotal);
+}
+
+// Modo Dark-Light 
 const botonFondo = document.getElementById("botonFondo");
 
 botonFondo.addEventListener("click", ()=>{
-    document.body.classList.toggle("dark");
-    if(document.body.classList.contains("dark")){
-        localStorage.setItem("modo", "dark")
-    }else {
-        localStorage.setItem("modo", "light");
-    }
+  document.body.classList.toggle("dark");
+  if(document.body.classList.contains("dark")){
+      localStorage.setItem("modo", "dark")
+  }else {
+      localStorage.setItem("modo", "light");
+  }
 })
 
-// recuperamos el modo del localStorage 
+//modo del localStorage 
 
 const modo = localStorage.getItem("modo");
 if (modo === "dark"){
-    document.body.classList.add("dark");
+  document.body.classList.add("dark");
 }else{
-    document.body.classList.remove("dark");
+  document.body.classList.remove("dark");
 };
 
 
-  datosPrestamo();
+datosPrestamo(); 
